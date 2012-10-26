7c718358c098c90cf26803a6eb68c64817e2e9c1
MCM <MCM@server.local>
2012-10-26 12:12:37 +0200

    -

--- php/gallery.php
+++ php/gallery.php
@@ -1,70 +1,38 @@
 <?php
 function makeGallery($path, $directory, $seo){
 	$dir = $path . "/" . $directory;
-	$handle = opendir($dir);
-	while ($file = readdir($handle)){
-		if($file == '.' OR $file == '..' or $file == 'thumbs' or $file == '.DS_Store')
-			continue;
-		else
-			$result_array[]=$file;
-	}
 
-	$filecount = count(glob("" . $dir . "*.jpg"));
-
-	closedir($handle);
-	array_multisort($result_array, SORT_ASC);
-	$rows = 3;
-	$cols = 8;
-
-	if(isset($_GET['page'])){
-		$page = $_GET['page'];
-	}else{
-		$page = 0;
-	}
-
-	$num_pics = count($result_array);
-	$cells = $rows * $cols;
-	$pages = ceil($num_pics / $cells);
-	$num_pages = $pages - 1;
-	$output = array();
-	$slices = array();
-	$push = 0;
-	$showpage = $page + 1;
-
-	for($k=0;$k<$pages;$k++){
-		array_push($slices, $push);
-		$push = $push + $cells;
-	}
-
-	for($k=0;$k<$pages;$k++){
-		if($page == key($slices)){
-			$output = array_slice($result_array, current($slices), $cells);
+	$filearray = array(); 
+	if ($fil = opendir($dir)) {
+		while (($file = readdir($fil)) !== false) {
+			if ($file != "." && $file != ".." && $file != "thumbs" && $file != ".DS_Store") { 
+				$filearray[] = $file;
+				$page = empty($_GET['page']) ? 1 : $_GET['page'];
+				$num_per_page = 25;
+				$total_pages = ceil(count($filearray)/$num_per_page);
+			}
 		}
-		next($slices);
-	}
 
-	reset($output);
+	array_multisort($filearray, SORT_ASC);
+
 	print '<div class="container-fluid">
 		<div class="row">';
 	print "<ul id='photoswipe'  class='thumbnails'>";
 
-	for($i=0;$i<$rows;$i++){
-		for($j=0;$j<$cols;$j++){
-			if(current($output) != false){
-				$value = current($output);
+	for($i = ($page - 1) * $num_per_page; $i < $page * $num_per_page; $i++){
+				$value = basename($filearray[$i]);
 				$parts = Explode('.', $value);
     		$title = $parts[count($parts) - 2];
     		$parts = Explode('-', $title);
     		$title = $parts[1];
     		if ($title == "") $title = $value;
-				print "<li class='box'><a title='$title' rel='group' class='thumbnails' href='$dir/$value'><img width='$width' height='inherrit' src='$dir/thumbs/$seo-$value' /></a></li>";
-				next($output);
-			}else{
-				print "</ul>";
-			}
-		}
+    		if($value != "")
+					print "<li class='box'><a title='$title' rel='group' class='thumbnails' href='$dir/$value'><img width='$width' height='inherrit' src='$dir/thumbs/$seo-$value' /></a></li>";
+	}
+	closedir($fil);
 	}
 
+	print "</ul>";
 
 	$ref = $_SERVER['PHP_SELF'];
 
